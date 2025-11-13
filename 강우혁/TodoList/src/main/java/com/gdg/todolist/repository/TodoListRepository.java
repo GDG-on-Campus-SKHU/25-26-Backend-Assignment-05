ab3ba1f814f9c4fd4e21c4bbdc75950bfbaf74df
woohyuk0428 <whkang03@gmail.com>
2025-11-13 22:29:48 +0900

    feat : UserRepository 구현

---
+++ 강우혁/TodoList/src/main/java/com/gdg/todolist/repository/TodoListRepository.java
@@ -0,0 +1,7 @@
+package com.gdg.todolist.repository;
+
+import com.gdg.todolist.domain.TodoList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoListRepository extends JpaRepository<TodoList, Long> {
+}
---
+++ 강우혁/TodoList/src/main/java/com/gdg/todolist/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.gdg.todolist.repository;
+
+import com.gdg.todolist.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
