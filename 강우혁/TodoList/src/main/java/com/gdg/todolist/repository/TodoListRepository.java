0b766926ad9b12effb51160febdb663e5adf9438
woohyuk0428 <whkang03@gmail.com>
2025-11-14 18:48:21 +0900

    feat : findByUser 구현

--- 강우혁/TodoList/src/main/java/com/gdg/todolist/repository/TodoListRepository.java
+++ 강우혁/TodoList/src/main/java/com/gdg/todolist/repository/TodoListRepository.java
@@ -1,7 +1,11 @@
 package com.gdg.todolist.repository;
 
 import com.gdg.todolist.domain.TodoList;
+import com.gdg.todolist.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TodoListRepository extends JpaRepository<TodoList, Long> {
+    List<TodoList> findByUser(User user);
 }
